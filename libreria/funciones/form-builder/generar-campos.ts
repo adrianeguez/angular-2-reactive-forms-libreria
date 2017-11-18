89364a420435c24af81caaf51b990cb9dd00b194
Adrian Eguez <eadepto@hotmail.com>
2017-11-18 10:17:44 -0500

    generar campos

--- libreria/funciones/form-builder/generar-campos.ts
+++ libreria/funciones/form-builder/generar-campos.ts
@@ -1,15 +1,22 @@
 export function generarCampos(clase) {
-  const campos = {};
-  Object
-    .keys(clase)
-    .map(
-      nombrePropiedad => {
-        if (nombrePropiedad.includes('mensajesValidacion')) {
-          const nombreInput = clase[nombrePropiedad].nombreInput;
-          const formGroup = 'formGroup';
-          campos[nombreInput] = clase[formGroup].get(nombreInput).value;
-        }
-      }
-    );
-  return campos;
+    const campos = {};
+    Object
+        .keys(clase)
+        .map(
+            nombrePropiedad => {
+                if (nombrePropiedad.includes('mensajesValidacion')) {
+                    const nombreInput = clase[nombrePropiedad].nombreInput;
+                    const formGroup = 'formGroup';
+                    const valorCampo = clase[formGroup].get(nombreInput).value;
+                    if (clase[nombrePropiedad].eliminarMascara) {
+                        const resultado = clase[nombrePropiedad].eliminarMascara(valorCampo);
+                        campos[nombreInput] = resultado;
+                    }
+                    else {
+                        campos[nombreInput] = valorCampo;
+                    }
+                }
+            }
+        );
+    return campos;
 }
